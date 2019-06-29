7aa8a08026e6bd27b9d32aeecbd9131f40e6316c
Matt Gage <veteater@gmail.com>
2019-06-29 12:16:45 -0700

    cleared out template stuff and added snapshot.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let appElement;
+
+  beforeEach(() => {
+    appElement = document.createElement('div');
+  });
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(appElement);
+  });
+
+  it('renders without crashing', () => {
+    ReactDOM.render(<App />, appElement);
+  });
+
+  it('renders the starting text', () => {
+    ReactDOM.render(<App />, appElement);
+    expect(appElement).toMatchSnapshot();
+  })
 });
