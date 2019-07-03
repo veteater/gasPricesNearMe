aa5e5bbddefe21b8c8b3154a51701939cab7ac9e
Matt <veteater@gmail.com>
2019-07-02 21:53:16 -0700

    Wrote snapshot test for entire app.

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
     const app = shallow(<App />);
+    expect(app).toMatchSnapshot();
+  });
+
+  it('mounts will full matched snapshot', () => {
+    const app = mount(<App />);
     expect(app).toMatchSnapshot();
   });
 });
